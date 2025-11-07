d6b8661b99a8c2fe81d061c25aee1715e55906f9
Xdblaze <160746019+Xdblaze7115@users.noreply.github.com>
2025-11-07 18:38:08 +0000

    Refactor UI.lua for improved code clarity

--- UI.lua
+++ UI.lua
@@ -1,42 +1,42 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "Hyperion",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   Icon = 0,
    LoadingTitle = "Hyperion External",
    LoadingSubtitle = "By Xdblaze",
-   ShowText = "Hyperion", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+   ShowText = "Hyperion",
+   Theme = "Default",
 
-   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+   ToggleUIKeybind = "K",
 
    DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+   DisableBuildWarnings = false,
 
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = true, -- Create a custom folder for your hub/game
+      FolderName = true,
       FileName = "Hyperion"
    },
 
    Discord = {
-      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "TPWvr56gZz", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      Enabled = true,
+      Invite = "TPWvr56gZz",
+      RememberJoins = true
    },
 
-   KeySystem = false, -- Set this to true to use our key system
+   KeySystem = false,
    KeySettings = {
       Title = "Untitled",
       Subtitle = "Key System",
-      Note = "No Method Of Obtaining The Key Is Provided", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"XD"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Note = "No Method Of Obtaining The Key Is Provided",
+      FileName = "Key",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = {"XD"}
    }
 })
 
-local Tab = Window:CreateTab("Execute", "code") -- Title, Image
+local Tab = Window:CreateTab("Execute", "code")
 
 local sourcecode = ""
 local Input = Tab:CreateInput({
@@ -46,13 +46,33 @@ local Input = Tab:CreateInput({
    RemoveTextAfterFocusLost = false,
    Flag = "Source",
    Callback = function(Text)
-       sourcecode = text
+       sourcecode = Text
    end,
 })
 
 local Button = Tab:CreateButton({
    Name = "Execute",
    Callback = function()
-       loadstring(sourcecode, "execute")()
+       if sourcecode and sourcecode ~= "" then
+           local success, result = pcall(function()
+               return loadstring(sourcecode, "execute")()
+           end)
+           
+           if not success then
+               Rayfield:Notify({
+                   Title = "Execution Error",
+                   Content = result,
+                   Duration = 6.5,
+                   Image = 0
+               })
+           end
+       else
+           Rayfield:Notify({
+               Title = "Error",
+               Content = "No Code To Execute!",
+               Duration = 3,
+               Image = 0
+           })
+       end
    end,
 })
